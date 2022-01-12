e629090ddd00f52bf476f4089d3bd9eb9da607b9
adi-fullstack <adi@fullstack.ph>
2022-01-12 14:53:37 +0800

    Contact form page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,331 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Text from "../components/text"
 import { StaticImage } from "gatsby-plugin-image"
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  const colors = require('tailwindcss/colors')
+  
+  module.exports = {
+    // ...
+    theme: {
+      extend: {
+        colors: {
+          'warm-gray': colors.warmGray,
+          teal: colors.teal,
+        },
+      },
+    },
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+const offices = [
+  { id: 1, city: 'Los Angeles', address: ['4556 Brendan Ferry', 'Los Angeles, CA 90210'] },
+  { id: 2, city: 'New York', address: ['886 Walter Streets', 'New York, NY 12345'] },
+  { id: 3, city: 'Toronto', address: ['7363 Cynthia Pass', 'Toronto, ON N3Y 4H8'] },
+  { id: 4, city: 'London', address: ['114 Cobble Lane', 'London N1 2EF'] },
+]
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+export default function Example() {
+  return (
+    <div className="bg-white">
+      <div className="bg-stone-50">
+        <nav
+          className="relative max-w-7xl mx-auto flex items-center justify-between pt-6 px-6 xl:px-8"
+          aria-label="Global"
+        >
+          <div className="flex items-center flex-1">
+            <div className="flex items-center justify-between w-full lg:w-auto">
+              <a href="#">
+                <StaticImage
+                  src="../images/Remarkable.png"
+                  width={300}
+                  quality={95}
+                  formats={["auto", "webp", "avif"]}
+                  alt="Remarkable logo"
+                  style={{ marginBottom: `1.45rem` }}
+                />
+              </a>
+              <div className="-mr-2 flex items-center lg:hidden">
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
+      <main className="overflow-hidden">
+        {/* Header */}
+        <div className="bg-stone-50">
+          <div className="py-24 lg:py-30">
+            <div className="relative z-10 max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
+              <Text h1_title="Get in touch" />
+              <Text h1_detail="If you’re looking for assistance with a trademark reach out and we can get you connect you with someone who can help." />
+            </div>
+          </div>
+        </div>
 
-export default IndexPage
+        {/* Contact section */}
+        <section className="relative bg-stone-50 py-4 " aria-labelledby="contact-heading">
+          <div className="absolute w-full h-1/2 bg-warm-gray-50" aria-hidden="true" />
+          {/* Decorative dot pattern */}
+          <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <svg
+              className="absolute z-0 top-0 right-0 transform -translate-y-16 translate-x-1/2 sm:translate-x-1/4 md:-translate-y-24 lg:-translate-y-72"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+              aria-hidden="true"
+            >
+              <defs>
+                <pattern
+                  id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect x={0} y={0} width={4} height={4} className="text-zinc-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width={404} height={384} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+            </svg>
+          </div>
+          <div className="max-w-7xl pb-3 mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="relative bg-white shadow-xl">
+              <h2 id="contact-heading" className="sr-only">
+                Contact us
+              </h2>
+
+              <div className="grid grid-cols-1 lg:grid-cols-3">
+                {/* Contact information */}
+                <div className="relative overflow-hidden py-10 px-6 bg-gradient-to-b from-teal-500 to-teal-600 sm:px-10 xl:px-12 xl:py-6">
+                  {/* Decorative angle backgrounds */}
+                  <div className="absolute inset-0 pointer-events-none sm:hidden" aria-hidden="true">
+                    <svg
+                      className="absolute inset-0 w-full h-full"
+                      width={343}
+                      height={388}
+                      viewBox="0 0 343 388"
+                      fill="none"
+                      preserveAspectRatio="xMidYMid slice"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z"
+                        fill="url(#linear1)"
+                        fillOpacity=".1"
+                      />
+                      <defs>
+                        <linearGradient
+                          id="linear1"
+                          x1="254.553"
+                          y1="107.554"
+                          x2="961.66"
+                          y2="814.66"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop stopColor="#fff" />
+                          <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                        </linearGradient>
+                      </defs>
+                    </svg>
+                  </div>
+                  <div
+                    className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none sm:block lg:hidden"
+                    aria-hidden="true"
+                  >
+                    <svg
+                      className="absolute inset-0 w-full h-full"
+                      width={359}
+                      height={339}
+                      viewBox="0 0 359 339"
+                      fill="none"
+                      preserveAspectRatio="xMidYMid slice"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z"
+                        fill="url(#linear2)"
+                        fillOpacity=".1"
+                      />
+                      <defs>
+                        <linearGradient
+                          id="linear2"
+                          x1="192.553"
+                          y1="28.553"
+                          x2="899.66"
+                          y2="735.66"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop stopColor="#fff" />
+                          <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                        </linearGradient>
+                      </defs>
+                    </svg>
+                  </div>
+                  <div
+                    className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none lg:block"
+                    aria-hidden="true"
+                  >
+                    <svg
+                      className="absolute inset-0 w-full h-full"
+                      width={160}
+                      height={678}
+                      viewBox="0 0 160 678"
+                      fill="none"
+                      preserveAspectRatio="xMidYMid slice"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z"
+                        fill="url(#linear3)"
+                        fillOpacity=".1"
+                      />
+                      <defs>
+                        <linearGradient
+                          id="linear3"
+                          x1="192.553"
+                          y1="325.553"
+                          x2="899.66"
+                          y2="1032.66"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop stopColor="#fff" />
+                          <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                        </linearGradient>
+                      </defs>
+                    </svg>
+                  </div>
+                  <Text h3_title="Describe what you need" />
+                  <Text h3_detail="Give us an overview of who you are and what you need help with someone will be in touch within a business day or two." />
+                </div>
+
+                {/* Contact form */}
+                <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12 shadow-md">
+                  <h3 className="text-lg font-medium text-warm-gray-900">Send us a message</h3>
+                  <form action="#" method="POST" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                    <div>
+                      <label htmlFor="first-name" className="block text-sm font-medium font-sans text-warm-gray-900">
+                        First name
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          type="text"
+                          name="first-name"
+                          id="first-name"
+                          autoComplete="given-name"
+                          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 border-2 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label htmlFor="last-name" className="block text-sm font-medium font-sans text-warm-gray-900">
+                        Last name
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          type="text"
+                          name="last-name"
+                          id="last-name"
+                          autoComplete="family-name"
+                          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 border-2 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label htmlFor="email" className="block text-sm font-medium font-sans text-warm-gray-900">
+                        Email
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="email"
+                          name="email"
+                          type="email"
+                          autoComplete="email"
+                          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 border-2 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex justify-between">
+                        <label htmlFor="phone" className="block text-sm font-medium font-sans text-warm-gray-900">
+                          Phone
+                        </label>
+                        <span id="phone-optional" className="text-sm font-sans text-zinc-400">
+                          Optional
+                        </span>
+                      </div>
+                      <div className="mt-1">
+                        <input
+                          type="text"
+                          name="phone"
+                          id="phone"
+                          autoComplete="tel"
+                          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 border-2 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                          aria-describedby="phone-optional"
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                      <label htmlFor="subject" className="block text-sm font-medium font-sans text-warm-gray-900">
+                        Subject
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          type="text"
+                          name="subject"
+                          id="subject"
+                          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 border-2 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                      <div className="flex justify-between">
+                        <label htmlFor="message" className="block text-sm font-medium font-sans text-warm-gray-900">
+                          Message
+                        </label>
+                        <span id="message-max" className="text-sm font-sans text-zinc-400">
+                          Max. 500 characters
+                        </span>
+                      </div>
+                      <div className="mt-1">
+                        <textarea
+                          id="message"
+                          name="message"
+                          rows={4}
+                          className="py-3 px-4 block w-full shadow text-warm-gray-900 border-2 focus:ring-teal-500 focus:border-teal-500 border border-warm-gray-300 rounded-md"
+                          aria-describedby="message-max"
+                          defaultValue={''}
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-2 sm:flex sm:justify-end">
+                      <button
+                        type="submit"
+                        className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium font-sans text-white bg-teal-500 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 sm:w-auto"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+      </main>
+    </div>
+  )
+}
