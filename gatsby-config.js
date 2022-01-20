02a177a6eb170657ff4ae5c4eae1177c1b1b24b0
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-01-20 18:43:10 +0800

    robots.txt, sitemap (#12) (#13)
    * robots.txt, sitemap
    
    * fixed site url
    
    Co-authored-by: adi-fullstack <79141549+adi-fullstack@users.noreply.github.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,25 @@
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    // siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://remarkablelegalwebsitestaging.gatsbyjs.io/`,
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: '/'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: 'ia_archiver', disallow: '/' }]
+      }
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-postcss`,
@@ -29,7 +43,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.jpg`, // This path is relative to the root of the site.
+        icon: `src/images/favicon (1).png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
