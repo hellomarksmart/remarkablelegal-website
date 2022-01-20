982c819c32397786e28cefa98c7f5707f04e1a3e
adi-fullstack <adi@fullstack.ph>
2022-01-20 16:50:54 +0800

    robots.txt, sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     // siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-    siteUrl: `https://remarkablelegalwebsitestaging.gatsbyjs.io/`,
+    siteUrl: `https://www.example.com`,
   },
   plugins: [
     {
